f8bb32f6dfb2696a5414a6cf4d31819409a1cd40
Jonathan Harder <jonharder6@gmail.com>
2013-03-15 17:59:50 -0500

    Now using Gtk's built in file chooser for simplicities sake.

--- mednagui.c
+++ mednagui.c
@@ -1,21 +1,19 @@
 #include<gtk/gtk.h>
 
+
 static gboolean delete_event(GtkWidget *widget, GdkEvent *event, gpointer data) {
+	g_print("Bye bye!\n");
 	gtk_main_quit();
 	return FALSE;
 }
 
 int main(int argc, char *argv[]) {
-	GtkWidget *window;
-
 	gtk_init(&argc, &argv);
 
-	window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
-	gtk_window_set_title(GTK_WINDOW(window), "Mednagui");
+	GtkWidget *fileSelect = gtk_file_selection_new("Select a game.");
+	g_signal_connect(fileSelect, "destroy", G_CALLBACK(delete_event), NULL);
 
-	g_signal_connect(window, "delete-event", G_CALLBACK(delete_event), NULL);
-
-	gtk_widget_show(window);
+	gtk_widget_show(fileSelect);
 
 	gtk_main();
 
